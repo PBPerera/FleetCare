aeda4aa150f76772b9dfd6259372862c05f5c757
chathuri988 <168174254+chathuri988@users.noreply.github.com>
2025-10-30 12:07:24 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,13 @@
 import React from "react";
 import ForgotPasswordFlow from "./pages/ForgotPasswordFlow";
 import "./App.css";
-import TripAllocation from "./pages/TripAllocation/TripAllocation";
-import TripScheduling from "./pages/TripScheduling/TripShceduling";
-import MyRequests from "./pages/MyRequests/MyRequests";
-import VehicleRequest from "./pages/VehicleRequest/VehicleRequest";
+ 
 
 function App() {
   return <>
   <ForgotPasswordFlow />
-  <TripAllocation />
-  <TripScheduling />
-  <MyRequests /> ,
-  <VehicleRequest /> 
-  </> ;
+   
+</> ;
 }
 
 export default App;
