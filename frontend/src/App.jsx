367fce9cf2e80b70b0f12fcac3c9a4baf23c4b5c
PBPerera <kpbperera655@gmail.com>
2025-11-04 21:35:51 +0530

    Update forgot password front-end files

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,17 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import ForgotPasswordFlow from "./pages/ForgotPasswordFlow";
 import "./App.css";
 
 function App() {
-  return <ForgotPasswordFlow />;
+  return (
+    <Router>
+      <Routes>
+        
+        <Route path="/forgotpassword" element={<ForgotPasswordFlow />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
