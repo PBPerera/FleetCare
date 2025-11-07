40c104c84afe670d4fd31cdd208944d1ac827dc6
ShanilkaMalshanWijesekara <166281046+ShanilkaMalshanWijesekara@users.noreply.github.com>
2025-11-07 23:33:25 +0530

    Add Frontend and backend folders

---
+++ Frontend/src/components/Layout/Layout.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './Layout.css';
+
+
+const Layout = ({ title, children }) => {
+  return (
+    <div className="layout">
+      
+      <div className="main-content">
+        
+        <div className="page-content">
+          {children}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Layout;
