586b2307a4c238d4fe7b9cfde2ad7bf8bdb16a32
ShanilkaMalshanWijesekara <166281046+ShanilkaMalshanWijesekara@users.noreply.github.com>
2025-11-19 13:45:32 +0530

    frontend setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,5 @@
 import { Routes, Route, Navigate } from "react-router-dom";
 
-<<<<<<< HEAD
 import Onboard from "./pages/Onboard.jsx";
 import Login from "./pages/Login.jsx";
 import Signup from "./pages/Signup.jsx";
@@ -10,17 +9,6 @@ import AdminDashboard from "./pages/AdminDashboard.jsx";
 import StaffDashboard from "./pages/StaffDashboard.jsx";
 import NotificationCenter from "./pages/NotificationCenter.jsx";
 import NotificationStaff from "./pages/NotificationStaff.jsx";
-=======
-import Onboard from "./pages/Onboard";
-import Login from "./pages/Login";
-import Signup from "./pages/Signup";
-import ForgotPasswordFlow from "./pages/ForgotPasswordFlow";
-import Home from "./pages/Home";
-import AdminDashboard from "./pages/AdminDashboard";
-import StaffDashboard from "./pages/StaffDashboard";
-import NotificationCenter from "./pages/NotificationCenter";
-import NotificationStaff from "./pages/NotificationStaff";
->>>>>>> f228393dd7466d879628ceb235fb697fb0130342
 import VehicleRequest from "./pages/VehicleRequest/VehicleRequest";
 import TripScheduling from "./pages/TripScheduling/TripScheduling";
 import TripAllocation from "./pages/TripAllocation/TripAllocation";
@@ -30,20 +18,12 @@ import RepairApprove from './pages/RepairApprove';
 import AuditLog from './pages/AuditLog';
 
 
-<<<<<<< HEAD
 // ✅ Keep ONLY ONE of these, matching your actual filename:
 import NotificationManagement from "./pages/NotificationManagement.jsx";
 
 import UserManagement from "./pages/UserManagement.jsx";
 import Vehicles from "./pages/Vehicles.jsx";
 import DriverManagement from "./pages/DriverManagement.jsx";
-=======
-import NotificationManagement from "./pages/NotificationManagement";
-
-import UserManagement from "./pages/UserManagement";
-import Vehicles from "./pages/Vehicles";
-import DriverManagement from "./pages/DriverManagement";
->>>>>>> f228393dd7466d879628ceb235fb697fb0130342
 
 import "./App.css";
 
@@ -56,11 +36,7 @@ export default function App() {
       <Route path="/onboard" element={<Onboard />} />
       <Route path="/login" element={<Login />} />
       <Route path="/signup" element={<Signup />} />
-<<<<<<< HEAD
       <Route path="/forgot-password/*" element={<ForgotPasswordFlow />} />
-=======
-      <Route path="/forgotpassword" element={<ForgotPasswordFlow />} />
->>>>>>> f228393dd7466d879628ceb235fb697fb0130342
       <Route path="/home" element={<Home />} />
 
       {/* Dashboards */}
