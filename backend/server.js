309de9f143209e94bd948b2ac15523de225547f5
chathuri988 <168174254+chathuri988@users.noreply.github.com>
2026-02-02 13:12:26 +0530

    Updated server.js file and created three files under controller models routes folders for vehicle request form

--- backend/server.js
+++ backend/server.js
@@ -7,14 +7,15 @@ import connectDB from "./config/db.js";
 import authRoutes from "./routes/passwordRoutes.js";
 import driverFormRoutes from "./routes/driverFormRoutes.js";
 import vehicleFormRoutes from "./routes/vehicleFormRoutes.js";
-import vehicleRoute from  "./routes/vehicles.js";
+import vehicleRoute from "./routes/vehicles.js";
 import driverRoute from "./routes/driver.js";
 import loginauthRoute from "./routes/auth.js";
 //MaintenanceManagement Routes
 import maintenanceRoutes from "./routes/maintenance.js";
 import serviceRoutes from "./routes/services.js";
 import repairRoutes from "./routes/repairs.js";
-import auditRoutes from './routes/audit.js';
+import auditRoutes from "./routes/audit.js";
+import vehicleRequestRoutes from "./routes/vehicleRequest.js";
 
 const app = express();
 
@@ -36,30 +37,29 @@ app.use("/api/repairs", repairRoutes);
 
 //Health endpoints
 app.get("/api/health", (req, res) => {
-    res.json({ 
-      status: "OK", 
-      message: "FleetCare API is running",
-      timestamp: new Date().toISOString()
-    });
+  res.json({
+    status: "OK",
+    message: "FleetCare API is running",
+    timestamp: new Date().toISOString(),
   });
-  
+});
 
 //Register Maintenance Routes
 app.use("/api/maintenance", maintenanceRoutes);
 app.use("/api/services", serviceRoutes);
 app.use("/api/repairs", repairRoutes);
 
-app.use('/api/audit', auditRoutes);
+app.use("/api/audit", auditRoutes);
+app.use("/api/vehicleRequests", vehicleRequestRoutes);
 
 //Health endpoints
 app.get("/api/health", (req, res) => {
-    res.json({ 
-      status: "OK", 
-      message: "FleetCare API is running",
-      timestamp: new Date().toISOString()
-    });
+  res.json({
+    status: "OK",
+    message: "FleetCare API is running",
+    timestamp: new Date().toISOString(),
   });
-  
+});
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
